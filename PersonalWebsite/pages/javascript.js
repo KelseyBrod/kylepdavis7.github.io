d635db21f19a675023ee6a3cc7b72064edea48c1
kylepdavis7 <kpd9@duke.edu>
2022-11-11 01:33:00 -0500

    edits

--- PersonalWebsite/pages/javascript.js
+++ PersonalWebsite/pages/javascript.js
@@ -3,11 +3,28 @@ window.addEventListener('scroll', parallax);
 function parallax() {
     
     var scrolled = window.pageYOffset;
-    var parallax = document.querySelector(".parallax");
-    var parallaxh = document.querySelector(".parallaxh");
-    var coords = (scrolled * .8) + 'px';
-    var coords2 = (scrolled * .2) + 'px';
-    parallax.style.transform = 'translateY(' + coords + ')';
-    parallaxh.style.transform = 'translateX(' + coords2 + ')';
+    var parallax = document.querySelector(".newbox2");
+    var parallax2 = document.querySelector(".newbox3");
+    var parallax3 = document.querySelector(".newbox4");
+    if (scrolled>700){
+        var coords2 = ((scrolled-701)*-2) + 'px';
+        var coords3 = (scrolled) + 'px';
+    } else {
+        var coords2 = 0 ;
+        var coords3 = (scrolled) + 'px';
+    }
+    if (scrolled>1400){
+        var coords4 = ((scrolled-1401)*2) + 'px';
+        var coords5 = (scrolled) + 'px';
+    } else {
+        var coords4 = 0 ;
+        var coords5 = (scrolled) + 'px';
+    }
+    var coords = (scrolled)*2 + 'px';
+    var coords1 = (scrolled) + 'px';
     
+   
+    parallax.style.transform = 'translate(' + coords + ',' + coords1 + ')' ;
+    parallax2.style.transform = 'translate(' + coords2 + ',' + coords3 +')';
+    parallax3.style.transform = 'translate(' + coords4 + ',' + coords5 +')';
 };
