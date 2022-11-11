fca018bb311df1dc56ec1735a6a2a4b6d9935fb1
kylepdavis7 <kpd9@duke.edu>
2022-11-11 06:11:33 -0500

    Updates

--- PersonalWebsite/pages/javascript.js
+++ PersonalWebsite/pages/javascript.js
@@ -3,28 +3,241 @@ window.addEventListener('scroll', parallax);
 function parallax() {
     
     var scrolled = window.pageYOffset;
-    var parallax = document.querySelector(".newbox2");
+    var parallax0 = document.querySelector(".parallax0");
+    var coords0 = (scrolled)*0.5 +'px';
+    parallax0.style.transform = 'translateY(' + coords0 + ')' ;
+
+    var parallax1 = document.querySelector(".newbox2");
     var parallax2 = document.querySelector(".newbox3");
     var parallax3 = document.querySelector(".newbox4");
-    if (scrolled>700){
-        var coords2 = ((scrolled-701)*-2) + 'px';
-        var coords3 = (scrolled) + 'px';
-    } else {
-        var coords2 = 0 ;
-        var coords3 = (scrolled) + 'px';
+    var parallax4 = document.querySelector(".newbox5");
+    var parallax5 = document.querySelector(".newbox6");
+    var parallax6 = document.querySelector(".newbox7");
+    var parallax7 = document.querySelector(".newbox8");
+    var parallax8 = document.querySelector(".newbox9");
+    var parallax9 = document.querySelector(".newbox10");
+    var parallax10 = document.querySelector(".newbox11");
+    var parallax11 = document.querySelector(".newbox12");
+    var parallax12 = document.querySelector(".newbox13");
+    var parallax13 = document.querySelector(".newbox14");
+    var parallax14 = document.querySelector(".newbox15");
+    var parallax15 = document.querySelector(".newbox16");
+    var parallax16 = document.querySelector(".newbox17");
+    var parallax17 = document.querySelector(".newbox18");
+    var parallax18 = document.querySelector(".newbox19");
+    var parallax19 = document.querySelector(".newbox20");
+    var parallax20 = document.querySelector(".newbox21");
+    var parallax21 = document.querySelector(".newbox22");
+    var parallax22 = document.querySelector(".newbox23");
+    var parallax23 = document.querySelector(".newbox24");
+    var parallax24 = document.querySelector(".newbox25");
+    var parallax25 = document.querySelector(".newbox26");
+    var parallax26 = document.querySelector(".newbox27");
+    var parallax27 = document.querySelector(".newbox28");
+    var parallax28 = document.querySelector(".newbox29");
+    if (scrolled>350){
+        var coordsx1 = (scrolled-351)*2 + 'px';
+        var coordsy1 = (scrolled-351) + 'px';
+        var coordsy2 = (scrolled-351) + 'px';
+        var coordsy3 = (scrolled-351) + 'px';
+        var coordsy4 = (scrolled-351) + 'px';
+        var coordsy5 = (scrolled-351) + 'px';
+        var coordsy6 = (scrolled-351) + 'px';
+        var coordsy7 = (scrolled-351) + 'px';
+        var coordsy8 = (scrolled-351) + 'px';
+        var coordsy9 = (scrolled-351) + 'px';
+        var coordsy10 = (scrolled-351) + 'px';
+        var coordsy11 = (scrolled-351) + 'px';
+        var coordsy12 = (scrolled-351) + 'px';
+        var coordsy13 = (scrolled-351) + 'px';
+        var coordsy14 = (scrolled-351) + 'px';
+        var coordsy15 = (scrolled-351) + 'px';
+        var coordsy16 = (scrolled-351) + 'px';
+        var coordsy17 = (scrolled-351) + 'px';
+        var coordsy18 = (scrolled-351) + 'px';
+        var coordsy19 = (scrolled-351) + 'px';
+        var coordsy20 = (scrolled-351) + 'px';
+        var coordsy21 = (scrolled-351) + 'px';
+        var coordsy22 = (scrolled-351) + 'px';
+        var coordsy23 = (scrolled-351) + 'px';
+        var coordsy24 = (scrolled-351) + 'px';
+        var coordsy25 = (scrolled-351) + 'px';
+        var coordsy26 = (scrolled-351) + 'px';
+        var coordsy27 = (scrolled-351) + 'px';
+        var coordsy28 = (scrolled-351) + 'px';
     }
-    if (scrolled>1400){
-        var coords4 = ((scrolled-1401)*2) + 'px';
-        var coords5 = (scrolled) + 'px';
-    } else {
-        var coords4 = 0 ;
-        var coords5 = (scrolled) + 'px';
+    else{
+        var coordsx1 = 0
     }
-    var coords = (scrolled)*2 + 'px';
-    var coords1 = (scrolled) + 'px';
-    
-   
-    parallax.style.transform = 'translate(' + coords + ',' + coords1 + ')' ;
-    parallax2.style.transform = 'translate(' + coords2 + ',' + coords3 +')';
-    parallax3.style.transform = 'translate(' + coords4 + ',' + coords5 +')';
+    if (scrolled>800){
+        var coordsx2 = ((scrolled-801)*-2) + 'px';
+
+    } else {
+        var coordsx2 = 0 ;
+    }
+    if (scrolled>1250){
+        var coordsx3 = ((scrolled-1251)*2) + 'px';
+    } else {
+        var coordsx3 = 0 ;
+    }
+    if (scrolled>1700){
+        var coordsx4 = ((scrolled-1701)*-2) + 'px';
+    } else {
+        var coordsx4 = 0 ;
+    }
+    if (scrolled>2150){
+        var coordsx5 = ((scrolled-2151)*2) + 'px';
+    } else {
+        var coordsx5 = 0 ;
+    }
+    if (scrolled>2600){
+        var coordsx6 = (scrolled-2600)*-2 + 'px';
+       
+    } else {
+        var coordsx6 = 0 ;
+    }
+    if (scrolled>3050){
+        var coordsx7 = ((scrolled-3051)*2) + 'px';
+
+    } else {
+        var coordsx7 = 0 ;
+    }
+    if (scrolled>3500){
+        var coordsx8 = ((scrolled-3501)*-2) + 'px';
+
+    } else {
+        var coordsx8 = 0 ;
+    }
+    if (scrolled>3950){
+        var coordsx9 = ((scrolled-3951)*2) + 'px';
+ 
+    } else {
+        var coordsx9 = 0 ;
+    }
+    if (scrolled>4400){
+        var coordsx10 = ((scrolled-4401)*-2) + 'px';
+
+    } else {
+        var coordsx10 = 0 ;
+    }
+    if (scrolled>4850){
+        var coordsx11 = ((scrolled-4851)*2) + 'px';
+
+    } else {
+        var coordsx11 = 0 ;
+    }
+    if (scrolled>5300){
+        var coordsx12 = ((scrolled-5301)*-2) + 'px';
+    } else {
+        var coordsx12 = 0 ;
+    }
+    if (scrolled>5750){
+        var coordsx13 = ((scrolled-5750)*2) + 'px';
+    } else {
+        var coordsx13 = 0 ;
+    }
+    if (scrolled>6200){
+        var coordsx14 = ((scrolled-6200)*-2) + 'px';
+    } else {
+        var coordsx14 = 0 ;
+    }
+    if (scrolled>6650){
+        var coordsx15 = ((scrolled-6650)*2) + 'px';
+    } else {
+        var coordsx15 = 0 ;
+    }
+    if (scrolled>7100){
+        var coordsx16 = ((scrolled-7100)*-2) + 'px';
+    } else {
+        var coordsx16 = 0 ;
+    }
+    if (scrolled>7550){
+        var coordsx17 = ((scrolled-7550)*2) + 'px';
+    } else {
+        var coordsx17 = 0 ;
+    }
+    if (scrolled>8000){
+        var coordsx18 = ((scrolled-8000)*-2) + 'px';
+    } else {
+        var coordsx18 = 0 ;
+    }
+    if (scrolled>8450){
+        var coordsx19 = ((scrolled-8450)*2) + 'px';
+    } else {
+        var coordsx19 = 0 ;
+    }
+    if (scrolled>8900){
+        var coordsx20 = ((scrolled-8900)*-2) + 'px';
+    } else {
+        var coordsx20 = 0 ;
+    }
+    if (scrolled>9350){
+        var coordsx21 = ((scrolled-9350)*2) + 'px';
+    } else {
+        var coordsx21 = 0 ;
+    }
+    if (scrolled>9800){
+        var coordsx22 = ((scrolled-9800)*-2) + 'px';
+    } else {
+        var coordsx22 = 0 ;
+    }
+    if (scrolled>10250){
+        var coordsx23 = ((scrolled-10250)*2) + 'px';
+    } else {
+        var coordsx23 = 0 ;
+    }
+    if (scrolled>10700){
+        var coordsx24 = ((scrolled-10700)*-2) + 'px';
+    } else {
+        var coordsx24 = 0 ;
+    }
+    if (scrolled>11150){
+        var coordsx25 = ((scrolled-11150)*2) + 'px';
+    } else {
+        var coordsx25 = 0 ;
+    }
+    if (scrolled>11600){
+        var coordsx26 = ((scrolled-11600)*-2) + 'px';
+    } else {
+        var coordsx26 = 0 ;
+    }
+    if (scrolled>12050){
+        var coordsx27 = ((scrolled-12050)*2) + 'px';
+    } else {
+        var coordsx27 = 0 ;
+    }
+    if (scrolled>12500){
+        var coordsx28 = ((scrolled-12500)*-2) + 'px';
+    } else {
+        var coordsx28 = 0 ;
+    }
+
+    parallax1.style.transform = 'translate(' + coordsx1 + ',' + coordsy1 + ')' ;
+    parallax2.style.transform = 'translate(' + coordsx2 + ',' + coordsy2 +')';
+    parallax3.style.transform = 'translate(' + coordsx3 + ',' + coordsy3 +')';
+    parallax4.style.transform = 'translate(' + coordsx4 + ',' + coordsy4 +')';
+    parallax5.style.transform = 'translate(' + coordsx5 + ',' + coordsy5 +')';
+    parallax6.style.transform = 'translate(' + coordsx6 + ',' + coordsy6 +')';
+    parallax7.style.transform = 'translate(' + coordsx7 + ',' + coordsy7 +')';
+    parallax8.style.transform = 'translate(' + coordsx8 + ',' + coordsy8 +')';
+    parallax9.style.transform = 'translate(' + coordsx9 + ',' + coordsy9 +')';
+    parallax10.style.transform = 'translate(' + coordsx10 + ',' + coordsy10 +')';
+    parallax11.style.transform = 'translate(' + coordsx11 + ',' + coordsy11 +')';
+    parallax12.style.transform = 'translate(' + coordsx12 + ',' + coordsy12 +')';
+    parallax13.style.transform = 'translate(' + coordsx13 + ',' + coordsy13 +')';
+    parallax14.style.transform = 'translate(' + coordsx14 + ',' + coordsy14 +')';
+    parallax15.style.transform = 'translate(' + coordsx15 + ',' + coordsy15 +')';
+    parallax16.style.transform = 'translate(' + coordsx16 + ',' + coordsy16 +')';
+    parallax17.style.transform = 'translate(' + coordsx17 + ',' + coordsy17 +')';
+    parallax18.style.transform = 'translate(' + coordsx18 + ',' + coordsy18 +')';
+    parallax19.style.transform = 'translate(' + coordsx19 + ',' + coordsy19 +')';
+    parallax20.style.transform = 'translate(' + coordsx20 + ',' + coordsy20 +')';
+    parallax21.style.transform = 'translate(' + coordsx21 + ',' + coordsy21 +')';
+    parallax22.style.transform = 'translate(' + coordsx22 + ',' + coordsy22 +')';
+    parallax23.style.transform = 'translate(' + coordsx23 + ',' + coordsy23 +')';
+    parallax24.style.transform = 'translate(' + coordsx24 + ',' + coordsy24 +')';
+    parallax25.style.transform = 'translate(' + coordsx25 + ',' + coordsy25 +')';
+    parallax26.style.transform = 'translate(' + coordsx26 + ',' + coordsy26 +')';
+    parallax27.style.transform = 'translate(' + coordsx27 + ',' + coordsy27 +')';
+    parallax28.style.transform = 'translate(' + coordsx28 + ',' + coordsy28 +')';
 };
